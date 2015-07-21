16485380d52b411d3d9d0d2440ccf2cfff0c0a38
mr.diko <mr.diko@gmail.com>
2015-07-21 20:51:35 +0300

    User authentication code complete

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once 'resourse/session.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +9,10 @@
 
 <h2>User Authentication System </h2>
 <hr>
-
+<?php if(!isset($_SESSION['username'])): ?>
 <p>You are currently not signin <a href="login.php">login</a> Not yet a member? <a href="signup.php">Signup</a></p>
-<p>Tou are logged in as {username} <a href="logout.php">Logout</a></p>
-
+<?php else : ?>
+<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username'] ."." ?> <a href="logout.php">Logout</a></p>
+<?php endif ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once "resourse/session.php";
 include_once "resourse/Database.php";
 include_once "resourse/utilitis.php";
@@ -13,14 +13,33 @@ if(isset($_POST['loginBtn'])) {
 	$form_errors = array_merge($form_errors, check_empty_fields($required_fields));
 
 	if (empty($form_errors)) {
-			// check if user exist in the database
+		// collect form data
+		$user = $_POST['username'];
+		$password = $_POST['password'];
+		// check if user exist in the database
+		$sqlQuery = "SELECT * FROM users WHERE username = :username";
+		$statement = $db->prepare($sqlQuery);
+		$statement->execute(array(':username' => $user));
 
-		}else{
-			if(count($form_errors) == 1) {
-				$result = "<p style='color: red;'>There was one error in the form</p>";
+		while ($row = $statement->fetch()) {
+			$id = $row['id'];
+			$hashed_password = $row['password'];
+			$username = $row['username'];
+		}
+			if(password_verify($password, $hashed_password="")) {
+				$_SESSION['id'] = $id;
+				$_SESSION['username'] = $username;
+				header('Location: index.php');
 			}else{
-				$result = "<p style='color: red;'>There was ".count($form_errors)." errors in the form</p>";
+				// header('Location: index.php');
+				$result = "<p style='color: red; border: 1px solid #808080; padding: 10px;'>Invalid username or password</p>";
 			}
+	}else{
+		if(count($form_errors) == 1) {
+			$result = "<p style='color: red;'>There was one error in the form</p>";
+		}else{
+			$result = "<p style='color: red;'>There was ".count($form_errors)." errors in the form</p>";
+		}
 	}	
 }
 
